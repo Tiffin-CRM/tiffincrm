5704cfbdd1555bf0d09ab316a5e15cf37079dc73
Devender Singh <99714664+DevendersinghRajput@users.noreply.github.com>
2024-03-17 18:43:24 +0530

    new file uplode

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+    document.body.classList.toggle("noscroll"); 
+}
+function toggleBtn() {
+    const background_pop_up = document.querySelector('.background_pop_up');
+    background_pop_up.classList.toggle('active');
+    document.body.classList.toggle("noscroll"); 
+}
