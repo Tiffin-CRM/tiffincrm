be24ca588f70060e40a1fc952b16bdc11f5be5b7
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-18 09:59:17 +0530

    js functioning

--- js/script.js
+++ js/script.js
@@ -1,10 +1,68 @@
 function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-    document.body.classList.toggle("noscroll"); 
-}
-function toggleBtn() {
-    const background_pop_up = document.querySelector('.background_pop_up');
-    background_pop_up.classList.toggle('active');
-    document.body.classList.toggle("noscroll"); 
-}
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+    document.body.classList.toggle("noscroll");
+  }
+  function toggleBtn() {
+    const background_pop_up = document.querySelector(".background_pop_up");
+    background_pop_up.classList.toggle("active");
+    document.body.classList.toggle("noscroll");
+  }
+  
+  function cancel_delivery() {
+    var popupTitle = "Cancel This Delivery";
+    var descText = "Are you sure you want to cancel this delivery?";
+    var noteText = "Note: This will only cancel this delivery";
+    var btn_text = "Yes, Cancel";
+    changeElement(popupTitle, descText, noteText, btn_text);
+  }
+  function pause_plan() {
+    var popupTitle = "Pause All Deliveries";
+    var descText = "Are you sure you want to pause all future deliveries?";
+    var noteText =
+      "Note: You have to manually resume the deliveries after pausing.";
+    var btn_text = "Yes, Pause All";
+    changeElement(popupTitle, descText, noteText, btn_text);
+  }
+  function request_edit() {
+    var popupTitle = "Request Edit in Meal Plan";
+    var descText =
+      "You have to request this to Soumya’s Kitchen Management Team on Whatsapp?";
+    var noteText =
+      "Note: Make sure to check the updated information here after requesting.";
+    var btn_text = "Send Message on Whatspp";
+    changeElement(popupTitle, descText, noteText, btn_text);
+  }
+  
+  function add_balance() {
+    var popupTitle = "Add Balance in Wallet";
+    var descText = "Must Follow Below Steps to Add Balance";
+    var noteText =
+      "Remember: You must have to send payment screenshot to Soumya’sK Kitchen to Add the Balance in your wallet";
+    var btn_text = "Pay Now";
+    changeElement(popupTitle, descText, noteText, btn_text);
+  
+    // Individual params
+    var btn_icon = "img/whast-app-btn-icon.svg";
+    document.getElementById("btn_icon").src = btn_icon;
+    document.getElementById("send_screenshot_link").style.display = "block";
+    document.getElementById("steps_desc").style.display = "block";
+    document.getElementById("amount_input_box").style.display = "block";
+  }
+  
+  function changeElement(popupTitle, descText, noteText, btn_text) {
+    document.getElementById("popupTitle").innerHTML = popupTitle;
+    document.getElementById("descText").innerHTML = descText;
+    document.getElementById("noteText").innerHTML = noteText;
+    document.getElementById("btn_text").innerHTML = btn_text;
+    popup_default();
+    toggleBtn();
+  }
+  
+  function popup_default() {
+    document.getElementById("send_screenshot_link").style.display = "none";
+    document.getElementById("steps_desc").style.display = "none";
+    document.getElementById("btn_icon").style.display = "none";
+    document.getElementById("amount_input_box").style.display = "none";
+  }
+  
