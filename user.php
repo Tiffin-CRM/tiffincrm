2e332fd41ded1843597c835c31ff797408e6da68
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2024-03-27 21:35:23 +0530

    Initialized API

--- user.php
+++ user.php
@@ -11,7 +11,9 @@ try {
 
     include("php/api.php");
     $user = getCustomer("58");
+    var_dump($user);
     $deliveries = getOrders($user["id"]);
+    var_dump($deliveries);
     //code...
 } catch (\Throwable $th) {
     echo $th->getMessage();
