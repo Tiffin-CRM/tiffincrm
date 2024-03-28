9e7ff04429e4b1f21ac41a8a5018768b094c7496
Rahul Mittal <78584898+rahulmitta1@users.noreply.github.com>
2024-03-28 12:33:59 +0530

    Updated

--- user.php
+++ user.php
@@ -9,7 +9,7 @@ $phone = $phonetoken / '4578348';
 
 try {
 
-    include("php/api.php");
+    include ("php/api.php");
     $user = getCustomer("58");
     $deliveries = getOrders($user["id"]);
     //code...
@@ -44,9 +44,9 @@ try {
             <div class="nav_logo">
                 <span>iMeals</span>
             </div>
-             <a href="https://api.whatsapp.com/send?phone=919068062563" class="whats_app_icon">
-                    <img src="img/whast-app-btn-icon.svg" alt="Whats app icon">
-                </a>
+            <a href="https://api.whatsapp.com/send?phone=919068062563" class="whats_app_icon">
+                <img src="img/whast-app-btn-icon.svg" alt="Whats app icon">
+            </a>
         </div>
         </div>
         <nav class="menu">
@@ -67,8 +67,10 @@ try {
                     <div class="name_deliry_text">Soumya Kitchen</div>
                 </div>
                 <div class="hero_cont_box">
-                    <div class="cont_box_text">Hi <?php echo $phone; ?>, Welcome to Soumya Kitchen. Use iMeals app to manage your meals
-                        and pay to Soumya kitchen. </div>
+                    <div class="cont_box_text">Hi
+                        <?php echo $phone; ?>, Welcome to Soumya Kitchen. Use iMeals app to manage your meals
+                        and pay to Soumya kitchen.
+                    </div>
                     <div class="app_installation_guide_box r-flex ali-c jut-c">
                         <a href="#">See Installation Guide</a> <span> (App size: 2 MB only)</span>
                     </div>
@@ -80,38 +82,42 @@ try {
                     </div>
                     <?php foreach ($deliveries as $delivery) {
                         if ($delivery['status'] == 'preparing') {
-                            ?> 
-                                <div class="active_deliveries_box">
-                                    <div class="brk_tre_box r-flex ali-c jut-sb">
-                                        <div class="brk_box r-flex ali-c">
-                                            <img src="img/sun-set.svg" alt="sun set and date">
-                                            <span>BreakFast - Today</span>
-                                        </div>
-                                        <div class="pre_box r-flex ali-c">
-                                            <img src="img/pre.svg" alt="line up and down">
-                                            <span>Preparation Started</span>
-                                        </div>
+                            ?>
+                            <div class="active_deliveries_box">
+                                <div class="brk_tre_box r-flex ali-c jut-sb">
+                                    <div class="brk_box r-flex ali-c">
+                                        <img src="img/sun-set.svg" alt="sun set and date">
+                                        <span>
+                                            <?php echo $delivery['time']; ?> - Today
+                                        </span>
                                     </div>
-                                    <div class="meal_item_list r-flex ali-c">
-                                        <img src="img/meals_icon.svg" alt="meal icon">
-                                        <span>4 Chapati, Dal, Paneer, Raita</span>
-                                    </div>
-                                    <div class="req_can_box r-flex ali-c jut-sb">
-                                        <div class="req_box r-flex ali-c" onclick="request_edit();">
-                                            <img src="img/edite-icon.svg" alt="eddite text icon">
-                                            <span>Request Edit in this delivery</span>
-                                        </div>
-                                        <div class="can_box r-flex ali-c" onclick="cancel_delivery();">
-                                            <img src="img/cancel.svg" alt="cancel icon">
-                                            <span>Cancel This Delivery</span>
-                                        </div>
-                                    </div>
-
-                                    <div class="active_deloveries_note">
-                                        <b>Note:</b> Amount is Deducted for this Delivery and cannot be refunded. But You can still
-                                        cancel this to better utilisation of food prepared for you
+                                    <div class="pre_box r-flex ali-c">
+                                        <img src="img/pre.svg" alt="line up and down">
+                                        <span>Preparation Started</span>
                                     </div>
                                 </div>
+                                <div class="meal_item_list r-flex ali-c">
+                                    <img src="img/meals_icon.svg" alt="meal icon">
+                                    <span>
+                                        <?php echo $delivery['items']; ?>
+                                    </span>
+                                </div>
+                                <div class="req_can_box r-flex ali-c jut-sb">
+                                    <div class="req_box r-flex ali-c" onclick="request_edit();">
+                                        <img src="img/edite-icon.svg" alt="eddite text icon">
+                                        <span>Request Edit in this delivery</span>
+                                    </div>
+                                    <div class="can_box r-flex ali-c" onclick="cancel_delivery();">
+                                        <img src="img/cancel.svg" alt="cancel icon">
+                                        <span>Cancel This Delivery</span>
+                                    </div>
+                                </div>
+
+                                <div class="active_deloveries_note">
+                                    <b>Note:</b> Amount is Deducted for this Delivery and cannot be refunded. But You can still
+                                    cancel this to better utilisation of food prepared for you
+                                </div>
+                            </div>
                         <?php }
                     } ?>
                 </div>
@@ -119,33 +125,38 @@ try {
                     <div class="active_deliveries_heading r-flex ali-c">
                         <span>Upcoming Delivery</span>
                     </div>
-                    <div class="active_deliveries_box">
-                        <div class="brk_tre_box r-flex ali-c jut-sb">
-                            <div class="brk_box r-flex ali-c">
-                                <img src="img/sun-set.svg" alt="sun set and date">
-                                <span>Lunch - Today</span>
-                            </div>
-                            <div class="pre_box r-flex ali-c" style="color: #3E3D3D;">
-                                <img src="img/clock.svg" alt="line up and down">
-                                <span>Preparation Not Started</span>
-                            </div>
-                        </div>
-                        <div class="meal_item_list r-flex ali-c">
-                            <img src="img/meals_icon.svg" alt="meal icon">
-                            <span>4 Chapati, Dal, Paneer, Raita</span>
-                        </div>
-                        <div class="req_can_box r-flex ali-c jut-sb">
-                            <div class="req_box r-flex ali-c" onclick="request_edit();">
-                                <img src="img/color-edite-icon.svg" alt="eddite text icon">
-                                <span style="color: #5546AF;">Request Edit in this delivery</span>
-                            </div>
-                            <div class="can_box r-flex ali-c"  onclick="cancel_delivery()">
-                                <img src="img/color-cancel.svg" alt="cancel icon">
-                                <span style="color: #5546AF;">Cancel This Delivery</span>
-                            </div>
-                        </div>
+                    <?php foreach ($deliveries as $delivery) {
+                        if ($delivery['status'] == 'pending') {
+                            ?>
+                            <div class="active_deliveries_box">
+                                <div class="brk_tre_box r-flex ali-c jut-sb">
+                                    <div class="brk_box r-flex ali-c">
+                                        <img src="img/sun-set.svg" alt="sun set and date">
+                                        <span><?php echo $delivery['time']; ?> - Today</span>
+                                    </div>
+                                    <div class="pre_box r-flex ali-c" style="color: #3E3D3D;">
+                                        <img src="img/clock.svg" alt="line up and down">
+                                        <span>Preparation Not Started</span>
+                                    </div>
+                                </div>
+                                <div class="meal_item_list r-flex ali-c">
+                                    <img src="img/meals_icon.svg" alt="meal icon">
+                                    <span><?php echo $delivery['items']; ?></span>
+                                </div>
+                                <div class="req_can_box r-flex ali-c jut-sb">
+                                    <div class="req_box r-flex ali-c" onclick="request_edit();">
+                                        <img src="img/color-edite-icon.svg" alt="eddite text icon">
+                                        <span style="color: #5546AF;">Request Edit in this delivery</span>
+                                    </div>
+                                    <div class="can_box r-flex ali-c" onclick="cancel_delivery()">
+                                        <img src="img/color-cancel.svg" alt="cancel icon">
+                                        <span style="color: #5546AF;">Cancel This Delivery</span>
+                                    </div>
+                                </div>
 
-                    </div>
+                            </div>
+                        <?php }
+                    } ?>
                 </div>
 
                 <div class="pause_deliveries_btn">
@@ -167,7 +178,9 @@ try {
                         <div class="wallet_icon r-flex ali-c">
                             <img src="img/wallet-icon.svg" alt="wallet icon">
                             <div class="balance_box c-flex">
-                                <div class="amount_balance_text"><?php echo $user["balance"]; ?></div>
+                                <div class="amount_balance_text">
+                                    <?php echo $user["balance"]; ?>
+                                </div>
                                 <div class="balance_span_text">Balance</div>
                             </div>
                         </div>
@@ -240,18 +253,18 @@ try {
                             <img src="img/meals_icon.svg" alt="meal icon">
                             <span>4 Chapati, Dal, Paneer, Raita</span>
                         </div>
-                        <div class="req_can_box r-flex ali-c jut-sb" >
+                        <div class="req_can_box r-flex ali-c jut-sb">
                             <div class="req_box r-flex ali-c">
                                 <img src="img/tag-icon.svg" alt="tag icon">
                                 <span><i>Rs. 40/Meal</i></span>
                             </div>
                             <div class="can_box r-flex ali-c">
-                                <div class="eddite_meal_plan r-flex ali-c"  onclick="request_edit();">
+                                <div class="eddite_meal_plan r-flex ali-c" onclick="request_edit();">
                                     <img src="img/color-edite-icon.svg" alt="cancel icon">
                                     <span>Request Edit</span>
                                 </div>
                                 &nbsp;
-                                <div class="resume_meal_plan r-flex ali-c"  onclick="pause_plan();">
+                                <div class="resume_meal_plan r-flex ali-c" onclick="pause_plan();">
                                     <img src="img/resume-icon.svg" alt="resume icon">
                                     <span>Resume Now</span>
                                 </div>
@@ -277,24 +290,29 @@ try {
                         <img src="img/close_cross.svg" alt="cross icon">
                     </div>
                     <div class="pop_heading_text" id="popupTitle">Request Edit in Meal Plan</div>
-                    <div class="dark_sub_heading"  id="descText" >You have to request this to Soumya’s Kitchen Management Team on
+                    <div class="dark_sub_heading" id="descText">You have to request this to Soumya’s Kitchen Management
+                        Team on
                         Whatsapp</div>
-                    <div class="dark_sub_heading" id="steps_desc" style="text-align: start; font-size: 14px;display:none;">
+                    <div class="dark_sub_heading" id="steps_desc"
+                        style="text-align: start; font-size: 14px;display:none;">
                         1. Enter Payment Amount & Click Below Button
                         <br>
                         2. Pay via any UPI apps
                         <br>
                         3. Send Payment Screenshot to Soumya’s Kitchen
                     </div>
-                    <div class="short_not_box" id="noteText">Remember: You must have to send payment screenshot to Soumya’sK Kitchen
+                    <div class="short_not_box" id="noteText">Remember: You must have to send payment screenshot to
+                        Soumya’sK Kitchen
                         to Add the Balance in your wallet</div>
 
-                    <input type="number" name="amount" id="amount_input_box" placeholder="Enter Amount" class="amount_input_box" required>
+                    <input type="number" name="amount" id="amount_input_box" placeholder="Enter Amount"
+                        class="amount_input_box" required>
                     <button class="pop_main_btn r-flex ali-c jut-c" id="popup_action_btn" onclick="toggleBtn()">
                         <span pop_main_btn_text id="btn_text">Send Message on Whatspp</span>
                         <img src="img/whast-app-btn-icon.svg" id="btn_icon" alt="what's app icon">
                     </button>
-                    <a href="#" class="ss_on_whats_app_btn r-flex ali-c jut-c hideit" id="send_screenshot_link"> Send Screenshot on Whatsapp </a>
+                    <a href="#" class="ss_on_whats_app_btn r-flex ali-c jut-c hideit" id="send_screenshot_link"> Send
+                        Screenshot on Whatsapp </a>
                 </div>
             </div>
         </section>
