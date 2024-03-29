136d155ce238202c85f91e1fb733496a42bc28f1
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-29 19:16:24 +0530

    imp

--- user.php
+++ user.php
@@ -72,7 +72,7 @@ try {
                 </div>
                 <div class="hero_cont_box">
                     <div class="cont_box_text">Hi Name (
-                        <?php echo $phone; ?>), Welcome to Mom’s Canteen. Use iMeals app to manage your meals
+                        <?php echo $phone; ?>), Welcome to Mom’s Canteen. Use iMeals to manage your meals
                         and pay to Mom’s Canteen.
                     </div>
                     <!-- <div class="app_installation_guide_box r-flex ali-c jut-c">
