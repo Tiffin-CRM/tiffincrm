ec33e49561e5b2aa567cd389df763dfb86554790
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-30 20:57:52 +0530

    favicons

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>TiffinCRM - Tiffin Service Management Application/Software</title>
 
-    <link rel="icon" href="img/favicon.svg" sizes="any">
-    <link rel="apple-touch-icon" href="img/favicon.svg">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png">
+<link rel="manifest" href="/img/icons/site.webmanifest">
+<link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#1205a3">
+<link rel="shortcut icon" href="/img/icons/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="/img/icons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+
     <meta name="theme-color" content="#5546af" />
 
     <link rel="stylesheet" href="css/base.css?0.0.7" />
