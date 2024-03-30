0dde9bd986c7642b61ea42ae9fe0bbd79fa31866
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-31 05:08:17 +0530

    imp

--- terms-of-use.php
+++ terms-of-use.php
@@ -18,37 +18,37 @@
           Welcome to <a href="https://tiffincrm.com">TiffinCrm</a>, a cutting-edge tiffin service management application. These Terms of Use govern your use of our application and services. By accessing or using TiffinCrm, you agree to comply with these terms and conditions.
         </p>
 
-<h2 class="sub_heading">1. Acceptance of Terms</h2>
+<h2 class="content_heading">1. Acceptance of Terms</h2>
 <p class="steps_items_desc">
   Welcome to TiffinCrm! These Terms of Use ("Terms") govern your access to and use of the TiffinCrm application ("App") and related services. By accessing or using the App, you agree to be bound by these Terms.
 </p>
 
-<h2 class="sub_heading">2. Use of TiffinCrm</h2>
+<h2 class="content_heading">2. Use of TiffinCrm</h2>
 <p class="steps_items_desc">
   TiffinCrm is designed to assist tiffin service providers in managing their businesses more effectively. You agree to use the App only for lawful purposes and in accordance with these Terms.
 </p>
 
-<h2 class="sub_heading">3. Prohibited Activities</h2>
+<h2 class="content_heading">3. Prohibited Activities</h2>
 <p class="steps_items_desc">
   You agree not to misuse TiffinCrm or engage in any activity that could harm, disable, or impair the functionality of the App. Prohibited activities include attempting to gain unauthorized access to our systems, interfering with the proper functioning of TiffinCrm, or violating any applicable laws or regulations.
 </p>
 
-<h2 class="sub_heading">4. User Accounts</h2>
+<h2 class="content_heading">4. User Accounts</h2>
 <p class="steps_items_desc">
   To access certain features of TiffinCrm, you may be required to create a user account. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.
 </p>
 
-<h2 class="sub_heading">5. Intellectual Property</h2>
+<h2 class="content_heading">5. Intellectual Property</h2>
 <p class="steps_items_desc">
   The content and materials provided through TiffinCrm, including but not limited to text, graphics, logos, and software, are owned by or licensed to us and are protected by copyright and other intellectual property laws.
 </p>
 
-<h2 class="sub_heading">6. Changes to Terms</h2>
+<h2 class="content_heading">6. Changes to Terms</h2>
 <p class="steps_items_desc">
   We reserve the right to update or modify these Terms at any time without prior notice. Changes will be effective immediately upon posting. Your continued use of TiffinCrm after any changes indicates your acceptance of the revised Terms.
 </p>
 
-<h2 class="sub_heading">7. Contact Us</h2>
+<h2 class="content_heading">7. Contact Us</h2>
 <p class="steps_items_desc">
   If you have any questions or concerns about these Terms of Use, please contact us at [insert contact details].
 </p>
