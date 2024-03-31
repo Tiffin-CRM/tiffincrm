64e3b0af0ab0585abd3e84388c165a8f72b0f970
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-31 09:44:50 +0530

    imp

--- download-app.php
+++ download-app.php
@@ -30,8 +30,8 @@
                     type="module"></script>
 
                 <dotlottie-player src="https://lottie.host/2332dc71-0002-48a4-8591-de33e936896d/qghooPzyI3.json"
-                    background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay
-                    style="margin: 20px auto;"></dotlottie-player>
+                    background="transparent" speed="1" style="width: 300px; height: 300px;margin: 20px auto;" loop
+                    autoplay></dotlottie-player>
 
                 <h1 class="content_heading">Request Early Access</h1>
                 <p class="steps_items_desc">
