2897d417a6da75ca61d3a7d0722994035f686af5
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-31 05:58:09 +0530

    imp

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,8 @@
         <div class="container">
           <div class="footer_contaier r-flex ali-c jut-sb">
             <div class="footer_logo">
-              <img src="/assets/img/logo2.svg" alt="" />
+              <a href="">
+              <img src="/assets/img/logo2.svg" alt="Tiffin Service Business Management App TiffinCRM Logo" /></a>
             </div>
             <div class="footer_copyright">
               Copyright © 2024 TiffinCRM | <a href="privacy-policy.php">Privacy</a> | <a href="terms-of-use.php">Terms</a>
