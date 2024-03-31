57777929d0f869489675a4c83d262d230ba167eb
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-31 06:43:31 +0530

    imp

--- download-app.php
+++ download-app.php
@@ -24,7 +24,7 @@
 <body>
     <?php include 'inc/header.php'; ?>
     <main>
-        <section class="main_content_area" style="margin-bottom: 50px;">
+        <section class="main_content_area" style="margin-bottom: 500px;">
             <div class="container">
                 <h1 class="content_heading">Request Early Access</h1>
                 <p class="steps_items_desc">
