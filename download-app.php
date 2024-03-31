d56dc4ae639dc208d7ec970ef8d3688d786e2d71
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-31 09:41:55 +0530

    lottie

--- download-app.php
+++ download-app.php
@@ -26,6 +26,12 @@
     <main>
         <section class="main_content_area" style="margin-bottom: 200px;">
             <div class="container">
+                <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+                    type="module"></script>
+
+                <dotlottie-player src="https://lottie.host/2332dc71-0002-48a4-8591-de33e936896d/qghooPzyI3.json"
+                    background="transparent" speed="1" style="width: 300px; height: 300px;" loop
+                    autoplay></dotlottie-player>
                 <h1 class="content_heading">Request Early Access</h1>
                 <p class="steps_items_desc">
                     TiffinCRM is not launched publically yet. </p>
