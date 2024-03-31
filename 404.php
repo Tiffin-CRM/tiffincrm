edc1a444da351f4d598018b7749ac08bae3eac9a
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-03-31 10:44:46 +0530

    404

---
+++ 404.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'inc/head.php'; ?>
+    <title>404 - TiffinCRM</title>
+    <meta name="description" content="Contact TiffinCrm for any inquiries, feedback, or support.">
+    <link rel="canonical" href="https://tiffincrm.com/404.php" />
+    <meta name="robots" content="noindex,nofollow">
+
+    <style>
+        .content_heading {
+            text-align: center;
+        }
+
+        .steps_items_desc {
+            text-align: center;
+        }
+
+        .play_stor_link {
+            background-color: #075E54;
+            max-width: 400px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include 'inc/header.php'; ?>
+    <main>
+        <section class="main_content_area" style="margin-bottom: 100px;padding-top: 30px;">
+            <div class="container">
+                <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+                    type="module"></script>
+
+                <dotlottie-player src="https://lottie.host/2332dc71-0002-48a4-8591-de33e936896d/qghooPzyI3.json"
+                    background="transparent" speed="1" style="width: 300px; height: 300px;margin: 20px auto;" loop
+                    autoplay></dotlottie-player>
+
+                <h1 class="content_heading">404 Not Found</h1>
+                <p class="steps_items_desc">
+                    The Page you are looking for is not found.</p>
+
+                <div class="left_side_btn_box r-flex ali-c gap-1 jut-c">
+
+                    <a href="/ target=" _blank" class="play_stor_link r-flex ali-c gap-1">
+                        <div class="play_stor_icon_box  r-flex ali-c gap-1">
+
+                        </div>
+                        <span>Go To HomePage</span>
+                    </a>
+
+                </div>
+            </div>
+        </section>
+        <?php include 'inc/footer.php'; ?>
+    </main>
+</body>
+
+</html>
