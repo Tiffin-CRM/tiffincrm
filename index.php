55a2a9d3a99c976d18e37e16c8a98ea9e3cc4ad5
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-04-13 04:09:08 +0530

    imp

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
                   <img src="/assets/img/close.svg" alt="crose icon" />
                 </div>
                 <div class="vdo_pop_box r-flex ali-c jut-c">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/J3OwLp3t6bA?si=xi9Q05NlMd-1IdEw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                  <img src="/assets/img/thumbnail2.jpg" alt="" />
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/J3OwLp3t6bA?si=xi9Q05NlMd-1IdEw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                  <img src="/assets/img/thumbnail2.jpg" alt="" />
                 </div>
               </div>
             </div>
