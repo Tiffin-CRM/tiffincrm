d05fa68434d892e23ab9625557abfd21048af071
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-04-21 16:36:04 +0530

    imp

--- inc/head.php
+++ inc/head.php
@@ -1,14 +1,10 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/icons/apple-touch-icon.png" />
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icons/favicon-16x16.png" />
-<link rel="manifest" href="/assets/img/icons/site.webmanifest" />
-<link rel="mask-icon" href="/assets/img/icons/safari-pinned-tab.svg" color="#1205a3" />
-<link rel="shortcut icon" href="/assets/img/icons/favicon.ico" />
-<meta name="msapplication-TileColor" content="#da532c" />
-<meta name="msapplication-config" content="/assets/img/icons/browserconfig.xml" />
+<link rel="icon" href="/assets/img/icons/favicon.ico" sizes="any">
+ <link rel="apple-touch-icon" href="/assets/img/icons/apple-touch-icon.png">
+ <link rel="manifest" href="/assets/img/icons/manifest.json">
+
 <meta name="theme-color" content="#5546af" />
 <link rel="stylesheet" href="/assets/css/base.css?0.0.7" />
 <link rel="stylesheet" href="/assets/css/style.css?<?php echo filemtime('assets/css/style.css'); ?>" />
