a196195b9c3245b2d2684c7802877937b42861a7
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-04-21 17:20:49 +0530

    imp

--- guides/how-to-setup.php
+++ guides/how-to-setup.php
@@ -98,7 +98,7 @@
         </h2>
         <p class="steps_items_desc">
             Setup Tiffin CRM & Organise Your Businesses. <br> 
-            <div class="articel_links">Helpful: <a href="https://tiffincrm.com/guides/how-to-use.php">How to Use tiffin Crm</a> </div></p>
+            <div class="articel_links">Must Read: <a href="https://tiffincrm.com/guides/how-to-use.php">How to Use tiffin Crm</a> </div></p>
 
         <div class="steps">
             <h3>
