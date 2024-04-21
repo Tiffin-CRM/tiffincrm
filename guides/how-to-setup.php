90a13493b5c86549b58db740eaff19be0e6cd4b3
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-04-21 17:50:37 +0530

    imp

--- guides/how-to-setup.php
+++ guides/how-to-setup.php
@@ -141,7 +141,7 @@
             </h3>
             <p class="steps_items_desc">
                  You can add repeat or one time orders for every customers. You can set the customer frequency of orders like repeat on sunday only or repeat on sunday and tuesday only or the repeating daily.</p>
-            <div class="info_box">
+            <div class="gen_info_box">
                 <h4 class="gen_info_box_title">General Info</h4>
                 <div class="info_box_content">
                     <ol>
@@ -186,7 +186,7 @@
             </h3>
             <p class="steps_items_desc">
                 Add or Deduct Customer Balance
-            <div class="info_box">
+            <div class="gen_info_box">
                 <h4 class="gen_info_box_title">General Info</h4>
                 <div class="info_box_content">
                     <ol>
