d48b2c4dc25c467efa17649ea0c8d6a1ef0c52a4
digiheadway <99395515+digiheadway@users.noreply.github.com>
2024-04-21 18:49:52 +0530

    imp

--- guides/how-to-use.php
+++ guides/how-to-use.php
@@ -132,7 +132,7 @@
                             imeals.in
                             and
                             also pause/resume all his future deliveries if he isn’t blocked.</li>
-                        <li>Your only work is to Press Start Proccessing to deduct customer's balance and Press Deliver all button when orders got delivered.</li>
+                        <li>Your only work is to Press <b>Start Proccessing</b> to deduct customer's balance and Press <b>Deliver all</b> button when orders got delivered.</li>
                         <li>If any customer want to cancel or pause their account. make sure it is reflecting on your
                             side
                             as
